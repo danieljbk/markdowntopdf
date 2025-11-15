5a6a104788842952fedf8598975b8f14d737706d
Joonbeom Kwon <djbkwon@gmail.com>
2025-11-15 13:02:51 -0800

    feat: created optimized Open Graph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://markdown.kwon.ai',
   output: 'static', // Static site generation
   build: {
     format: 'file', // Generate index.html files
